2e61d6c434a8ad0b61705951f5b755ba980111c4
chuan <1516998446@qq.com>
2024-08-06 11:55:17 +0800

    完成前后端框架搭建

---
+++ FrontEnd/InterestCircleViteProject/tailwind.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: HHC
+ * @Date: 2024-08-06 11:45:01
+ * @LastEditTime: 2024-08-06 11:47:15
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+    "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
