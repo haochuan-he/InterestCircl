0915c44a7efcc10d0c8dafc235e47248e17ce78c
chuan <1516998446@qq.com>
2024-08-07 12:34:43 +0800

    login不成功

--- FrontEnd/InterestCircleViteProject/src/main.tsx
+++ FrontEnd/InterestCircleViteProject/src/main.tsx
@@ -1,10 +1,21 @@
+/*
+ * @Author: HHC
+ * @Date: 2024-08-06 11:31:42
+ * @LastEditTime: 2024-08-07 12:30:03
+ */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// import Login from "./components/Login.jsx"
+import Login from './Login.tsx'
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {/* <App /> */}
+    <Login />
+  </React.StrictMode >,
 )
--- FrontEnd/InterestCircleViteProject/tailwind.config.js
+++ FrontEnd/InterestCircleViteProject/tailwind.config.js
@@ -1,12 +1,12 @@
 /*
  * @Author: HHC
  * @Date: 2024-08-06 11:45:01
- * @LastEditTime: 2024-08-06 11:47:15
+ * @LastEditTime: 2024-08-07 12:26:04
  */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html",
-    "./src/**/*.{html,js}"],
+    "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
